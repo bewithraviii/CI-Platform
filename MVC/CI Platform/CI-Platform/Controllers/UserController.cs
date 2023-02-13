c8412ab26f76871041126d8a6d54a2f517b682c4
unknown <tatva@PCA159.server7.com>
2023-02-13 17:48:04 +0530

    Createed Forgot-Password page in MVC

---
+++ MVC/CI Platform/CI-Platform/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CI_Platform.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult LoginPage()
+        {
+            return View();
+        }
+
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        public IActionResult RegistrationPage()
+        {
+            return View();
+        }
+    }
+}
