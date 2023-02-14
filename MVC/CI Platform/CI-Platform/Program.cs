38a295b4e07c5d99377c8c044565cdb06e471ffc
unknown <tatva@PCA159.server7.com>
2023-02-14 18:20:04 +0530

    Completed Reset-Password page in MVC

--- MVC/CI Platform/CI-Platform/Program.cs
+++ MVC/CI Platform/CI-Platform/Program.cs
@@ -1,6 +1,5 @@
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
