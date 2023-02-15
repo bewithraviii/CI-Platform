c57687cb9b25a9ec4e3980e6bf306800da302b1e
unknown <tatva@PCA159.server7.com>
2023-02-15 18:24:11 +0530

    Completed Registration page in MVC

--- MVC/CI Platform/CI-Platform/Models/ErrorViewModel.cs
+++ MVC/CI Platform/CI-Platform/Models/ErrorViewModel.cs
@@ -2,6 +2,9 @@ namespace CI_Platform.Models
 {
     public class ErrorViewModel
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
